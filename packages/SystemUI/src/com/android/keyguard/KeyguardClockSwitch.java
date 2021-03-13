Kang-OS-R/android_frameworks_base

base: Refactor log
Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>

packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.os.Build;
+import android.os.SystemProperties;
 import android.transition.Fade;
 import android.transition.Transition;
 import android.transition.TransitionListenerAdapter;
@@ -716,9 +717,13 @@ public class KeyguardClockSwitch extends RelativeLayout {
 
         @Override
         public void onKeyguardVisibilityChanged(boolean showing) {
-            Log.d("Error", "event called");
+            if (SystemProperties.getBoolean("kangos.debug", false)) {
+                Log.d(TAG, "onKeyguardVisibilityChanged");
+            }
             if (!showing) {
-                Log.d("Error", "not showing");
+                if (SystemProperties.getBoolean("kangos.debug", false)) {
+                    Log.d(TAG, "onKeyguardVisibilityChanged value of boolean is false");
+                }
                 if (mAnimationStatus) {
                     SeamlessClockSwitch.blinkInfo(mChangeClockface, false, getCurrentTextColor(), mContext);
                     SeamlessClockSwitch.shakeClock(mSmallClockFrame, false);

packages/SystemUI/src/com/android/systemui/util/Utils.java
@@ -25,6 +25,8 @@ import android.view.View;
 
 import com.android.systemui.shared.system.QuickStepContract;
 import com.android.systemui.statusbar.CommandQueue;
+import android.os.SystemProperties;
+import android.util.Log;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -141,4 +143,9 @@ public class Utils {
                 Settings.Secure.MEDIA_CONTROLS_RESUME, 1);
         return useQsMediaPlayer(context) && flag > 0;
     }
+
+    public static boolean isKangosDebug() {
+        return SystemProperties.getBoolean("kangos.debug", false);
+    }
+
 }

Joker-commits/platform_frameworks_base

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.os.Build;
+import android.os.SystemProperties;
 import android.transition.Fade;
 import android.transition.Transition;
 import android.transition.TransitionListenerAdapter;
@@ -675,9 +676,13 @@ public class KeyguardClockSwitch extends RelativeLayout {
 
         @Override
         public void onKeyguardVisibilityChanged(boolean showing) {
-            Log.d("Error", "event called");
+            if (SystemProperties.getBoolean("coltos.debug", false)) {
+                Log.d(TAG, "onKeyguardVisibilityChanged");
+            }
             if (!showing) {
-                Log.d("Error", "not showing");
+                if (SystemProperties.getBoolean("coltos.debug", false)) {
+                    Log.d(TAG, "onKeyguardVisibilityChanged value of boolean is false");
+                }
                 if (mAnimationStatus) {
                     SeamlessClockSwitch.blinkInfo(mChangeClockface, false, getCurrentTextColor(), mContext);
                     SeamlessClockSwitch.shakeClock(mSmallClockFrame, false);

packages/SystemUI/src/com/android/systemui/util/Utils.java
@@ -25,6 +25,8 @@ import android.view.View;
 
 import com.android.systemui.shared.system.QuickStepContract;
 import com.android.systemui.statusbar.CommandQueue;
+import android.os.SystemProperties;
+import android.util.Log;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -141,4 +143,9 @@ public class Utils {
                 Settings.Secure.MEDIA_CONTROLS_RESUME, 1);
         return useQsMediaPlayer(context) && flag > 0;
     }
+
+    public static boolean iscoltosDebug() {
+        return SystemProperties.getBoolean("coltos.debug", false);
+    }
+
 }